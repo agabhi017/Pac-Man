289a2093ea6e2a213c83c0dbc1baf255f08d06fb
Abhishek Agarwal <agabhi001@gmail.com>
2021-03-22 20:08:57 +0530

    updated the application class

--- include/myApplication.h
+++ include/myApplication.h
@@ -17,30 +17,32 @@
 
 class myApplication {
     private:
-        sf::RenderWindow window_;
-        int default_window_width_;
-        int default_window_height_;
-        int current_level_;
-        int max_level_;
-        long long score_;
-        long long high_score_;
-        sf::VideoMode videomode_;
-        sf::Vector2f window_origin_;
-        sf::View view_;
-        std::string current_screen_type_;
-        sf::Vector2u min_window_size_;
-        myResources resources_;
-        myScreen welcome_screen_;
-        myScreen pre_game_screen_;
-        myScreen game_screen_;
-        myScreen end_screen_;
-        sf::Sound sound_;
-        bool wait_;
+        
+        int                 default_window_width_;
+        int                 default_window_height_;
+        int                 current_level_;
+        int                 max_level_;
+        long long           score_;
+        long long           high_score_;
+        bool                wait_;
+        sf::RenderWindow    window_;
+        sf::VideoMode       videomode_;
+        sf::Vector2f        window_origin_;
+        sf::View            view_;
+        std::string         current_screen_type_;
+        sf::Vector2u        min_window_size_;
+        myResources         resources_;
+        myScreen            welcome_screen_;
+        myScreen            pre_game_screen_;
+        myScreen            game_screen_;
+        myScreen            end_screen_;
+        sf::Sound           sound_;
     public:
         myApplication();
         void appInit(const int, const int);
         void setDefaultWindow(const int, const int);
         void configureWindow(const int, const int);
+        void loadAScreen(myScreen&, const std::string&);
         void loadScreen(const std::string&);
         void loadAllScreens();
         void updateView();
@@ -49,16 +51,17 @@ class myApplication {
         void setCurrentScreenType(const std::string&);
         void setCurrentLevel(int);
         void runApp();
-        void checkWindowClosed(sf::Event&);
-        void checkChangeScreen(sf::Event&);
-        void checkPacManVelocity(sf::Event&);
+        void checkWindowClosed(const sf::Event&);
+        void switchScreen(const std::string&, const std::string&);
+        void checkChangeScreen(const sf::Event&);
+        void checkPacManVelocity(const sf::Event&);
         void checkPacManVelocity();
         void updateScores();
         void setMinWindowSize(sf::Vector2u);
         void resetApp();
         void checkLevelClear();
         void checkGameOver();
-        void checkWindowFocus(sf::Event&);
+        void checkWindowFocus(const sf::Event&);
         void setSound(const std::string&);
         const std::string& getCurrenScreenType(); 
         const int getCurrentLevel();
@@ -71,7 +74,7 @@ class myApplication {
         sf::Vector2f& getWindowOrigin();
         int getDefaultWindowHeight();
         int getDefaultWindowWidth();
-        std::vector <int> getLevel(int);
+        const std::vector <int> getLevel(int);
         int getScore();
         int getHighScore();
         sf::Sound& getSound();
--- src/myApplication.cpp
+++ src/myApplication.cpp
@@ -3,7 +3,7 @@
 
 myApplication::myApplication(){}
 
-void myApplication::setMinWindowSize(sf::Vector2u size){
+void myApplication::setMinWindowSize(const sf::Vector2u size){
     min_window_size_ = size;
 }
 
@@ -12,23 +12,17 @@ void myApplication::setDefaultWindow(const int width, const int height){
     default_window_height_ = height;
 }
 
+void myApplication::loadAScreen(myScreen& screen_object, const std::string& type){
+    //loads a screen object in the memory given the type
+    screen_object.screenInit(*this, type);
+    screen_object.renderScreen(type, *this);
+}
+
 void myApplication::loadScreen(const std::string& type){
-    if (type == "welcome"){
-        welcome_screen_.screenInit(*this, type);
-        welcome_screen_.renderScreen(type, *this);
-    }
-    else if (type == "end"){
-        end_screen_.screenInit(*this, type);
-        end_screen_.renderScreen(type, *this);
-    }
-    else if (type == "pre_game"){
-        pre_game_screen_.screenInit(*this, type);
-        pre_game_screen_.renderScreen(type, *this);
-    }
-    else if (type == "game"){
-        game_screen_.screenInit(*this, type);
-        game_screen_.renderScreen(type, *this);
-    }
+    if (type == "welcome")          {this->loadAScreen(welcome_screen_, type);}
+    else if (type == "end")         {this->loadAScreen(end_screen_, type);}
+    else if (type == "pre_game")    {this->loadAScreen(pre_game_screen_, type);}
+    else if (type == "game")        {this->loadAScreen(game_screen_, type);}
 }
 
 void myApplication::loadAllScreens(){
@@ -42,7 +36,7 @@ void myApplication::setCurrentScreenType(const std::string& type){
     current_screen_type_ = type;
 }
 
-void myApplication::setCurrentLevel(int level){
+void myApplication::setCurrentLevel(const int level){
     current_level_ = level;
 }
 
@@ -52,7 +46,7 @@ void myApplication::configureWindow(const int screen_width, const int screen_hei
     window_.create(videomode_, "PAC-MAN");
     window_.setVerticalSyncEnabled(true);
     window_.setFramerateLimit(12);
-    setMinWindowSize(sf::Vector2u(screen_width, screen_height));
+    this->setMinWindowSize(sf::Vector2u(screen_width, screen_height));
 }
 
 void myApplication::updateView(){
@@ -70,49 +64,47 @@ void myApplication::appInit(const int width, const int height){
     resources_.loadAllResources();
     configureWindow(default_window_width_, default_window_height_);
     max_level_ = 5;
-    setCurrentScreenType("welcome");
-    setCurrentLevel(1);
-    loadAllScreens();
-    updateWindowOrigin();
+    this->setCurrentScreenType("welcome");
+    this->setCurrentLevel(1);
+    this->loadAllScreens();
+    this->updateWindowOrigin();
     score_ = 0;
     high_score_ = 0;
-    setSound("welcome_sound_buff");
+    this->setSound("welcome_sound_buff");
     wait_ = false;
 }
 
 void myApplication::drawScreen(){
+    //checks to avoid window being resized beyond the min threshold
     if (window_.getSize().x < min_window_size_.x){
         window_.setSize(sf::Vector2u(min_window_size_.x, window_.getSize().y));
     }
     if (window_.getSize().y < min_window_size_.y){
        window_.setSize(sf::Vector2u(window_.getSize().x, min_window_size_.y));
     }
-    if (current_screen_type_ == "welcome"){
-        welcome_screen_.drawScreen(*this);
-    }
-    else if (current_screen_type_ == "end"){
-        end_screen_.drawScreen(*this);
-    }
-    else if (current_screen_type_ == "pre_game"){
-        pre_game_screen_.drawScreen(*this);
-    }
-    else if (current_screen_type_ == "game"){
-        game_screen_.drawScreen(*this);
-    }
+    if (current_screen_type_ == "welcome"){welcome_screen_.drawScreen(*this);}
+    else if (current_screen_type_ == "end"){end_screen_.drawScreen(*this);}
+    else if (current_screen_type_ == "pre_game"){pre_game_screen_.drawScreen(*this);}
+    else if (current_screen_type_ == "game"){game_screen_.drawScreen(*this);}
 }
 
-void myApplication::checkWindowClosed(sf::Event& event){
+void myApplication::checkWindowClosed(const sf::Event& event){
     if (event.type == sf::Event::Closed){
         sound_.stop();
         window_.close();
     }
 }
 
-void myApplication::checkChangeScreen(sf::Event& event){
+void myApplication::switchScreen(const std::string& new_screen, const std::string& sound_buffer){
+    this->setCurrentScreenType(new_screen);
+    this->loadScreen(current_screen_type_);
+    this->setSound(sound_buffer);
+}
+
+void myApplication::checkChangeScreen(const sf::Event& event){
+    //to check if a user has entered from one screen to another by pressing a key or by completing/failing a level
     if (current_screen_type_ == "welcome" && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Enter){
-        setCurrentScreenType("pre_game");
-        loadScreen(current_screen_type_);
-        setSound("interval_sound_buff");
+        this->switchScreen("pre_game", "interval_sound_buff");
     }
     else if (current_screen_type_ == "pre_game" && event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Enter){
         setCurrentScreenType("game");
@@ -123,68 +115,38 @@ void myApplication::checkChangeScreen(sf::Event& event){
     }
 }
 
-void myApplication::checkPacManVelocity(sf::Event& event){
+void myApplication::checkPacManVelocity(const sf::Event& event){
+    //check if the pacman has an active velocity on the basis of triggered events
     if (current_screen_type_ == "game"){
         if (event.type == sf::Event::KeyPressed){
-            if (event.key.code == sf::Keyboard::Up){
-                game_screen_.setVelocity(*this, "Up");
-            }
-            else if (event.key.code == sf::Keyboard::Down){
-                game_screen_.setVelocity(*this, "Down");
-            }
-            else if (event.key.code == sf::Keyboard::Right){
-                game_screen_.setVelocity(*this, "Right");
-            }
-            else if (event.key.code == sf::Keyboard::Left){
-                game_screen_.setVelocity(*this, "Left");
-            }
+            if (event.key.code == sf::Keyboard::Up)         {game_screen_.setVelocity(*this, "Up");}
+            else if (event.key.code == sf::Keyboard::Down)  {game_screen_.setVelocity(*this, "Down");}
+            else if (event.key.code == sf::Keyboard::Right) {game_screen_.setVelocity(*this, "Right");}
+            else if (event.key.code == sf::Keyboard::Left)  {game_screen_.setVelocity(*this, "Left");}
         }
         else if (event.type == sf::Event::KeyReleased){
-            if (event.key.code == sf::Keyboard::Up){
-                game_screen_.killVelocity("Up");
-            }
-            else if (event.key.code == sf::Keyboard::Down){
-                game_screen_.killVelocity("Down");
-            }
-            else if (event.key.code == sf::Keyboard::Right){
-                game_screen_.killVelocity("Right");
-            }
-            else if (event.key.code == sf::Keyboard::Left){
-                game_screen_.killVelocity("Left");
-            }
+            if (event.key.code == sf::Keyboard::Up)         {game_screen_.killVelocity("Up");}
+            else if (event.key.code == sf::Keyboard::Down)  {game_screen_.killVelocity("Down");}
+            else if (event.key.code == sf::Keyboard::Right) {game_screen_.killVelocity("Right");}
+            else if (event.key.code == sf::Keyboard::Left)  {game_screen_.killVelocity("Left");}
         }
     }
 }
 
 void myApplication::checkPacManVelocity(){
+    //check if the pacman has active velocity on the basis of real-time inputs
     if (current_screen_type_ == "game"){
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-            game_screen_.setVelocity(*this, "Left");
-        }
-        else {
-            game_screen_.killVelocity("Left");
-        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))     {game_screen_.setVelocity(*this, "Left");}
+        else                                                    {game_screen_.killVelocity("Left");}
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-            game_screen_.setVelocity(*this, "Up");
-        }
-        else {
-            game_screen_.killVelocity("Up");
-        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))       {game_screen_.setVelocity(*this, "Up");}
+        else                                                    {game_screen_.killVelocity("Up");}
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-            game_screen_.setVelocity(*this, "Down");
-        }
-        else {
-            game_screen_.killVelocity("Down");
-        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))     {game_screen_.setVelocity(*this, "Down");}
+        else                                                    {game_screen_.killVelocity("Down");}
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
-            game_screen_.setVelocity(*this, "Right");
-        }
-        else {
-            game_screen_.killVelocity("Right");
-        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))    {game_screen_.setVelocity(*this, "Right");}
+        else                                                    {game_screen_.killVelocity("Right");}
     }
 }
 
@@ -198,44 +160,30 @@ void myApplication::setSound(const std::string& buffer_name){
 void myApplication::checkLevelClear(){
     if (current_screen_type_ == "game" && getCurrentScreen().getLevelClearStatus() == true){
         if (current_level_ == max_level_){
-            setCurrentScreenType("end");
-            loadScreen(current_screen_type_);
-            setSound("end_sound_buff");
+            this->switchScreen("end", "end_sound_buff");
         }
         else {
-            updateScores();
-            setCurrentLevel(current_level_ + 1);
-            setCurrentScreenType("pre_game");
-            loadScreen(current_screen_type_);
-            setSound("interval_sound_buff");
+            this->updateScores();
+            this->setCurrentLevel(current_level_ + 1);
+            this->switchScreen("pre_game", "interval_sound_buff");
         }
     }
 }
 
 void myApplication::checkGameOver(){
     if (current_screen_type_ == "game" && getCurrentScreen().getGameOverStatus() == true){
-        setCurrentScreenType("end");
-        loadScreen(current_screen_type_);
-        std::cout << current_screen_type_ << std::endl;
-        setSound("interval_sound_buff");
-        std::cout << current_screen_type_ << std::endl;
+        this->switchScreen("end", "interval_sound_buff");
     }
 }
 
 void myApplication::updateScores(){
     score_ += getCurrentScreen().getScore();
-    if (score_ > high_score_){
-        high_score_ = score_;
-    }
+    if (score_ > high_score_)   {high_score_ = score_;}
 }
 
-void myApplication::checkWindowFocus(sf::Event& event){
-    if (event.type == sf::Event::LostFocus){
-        wait_ = true;
-    }
-    if (event.type == sf::Event::GainedFocus){
-        wait_ = false;
-    }
+void myApplication::checkWindowFocus(const sf::Event& event){
+    if (event.type == sf::Event::LostFocus)     {wait_ = true;}
+    if (event.type == sf::Event::GainedFocus)   {wait_ = false;}
 }
 
 void myApplication::runApp(){
@@ -292,18 +240,10 @@ const std::string& myApplication::getCurrenScreenType(){
 }
 
 myScreen& myApplication::getCurrentScreen(){
-    if (current_screen_type_ == "welcome"){
-        return welcome_screen_;
-    }
-    else if (current_screen_type_ == "end"){
-        return end_screen_;
-    }
-    else if (current_screen_type_ == "pre_game"){
-        return pre_game_screen_;
-    }
-    else if (current_screen_type_ == "game"){
-        return game_screen_;
-    }
+    if (current_screen_type_ == "welcome")          {return welcome_screen_;}
+    else if (current_screen_type_ == "end")         {return end_screen_;}
+    else if (current_screen_type_ == "pre_game")    {return pre_game_screen_;}
+    else if (current_screen_type_ == "game")        {return game_screen_;}
 }
 
 sf::Vector2f& myApplication::getWindowOrigin(){
@@ -318,7 +258,7 @@ int myApplication::getDefaultWindowHeight(){
     return default_window_height_;
 }
 
-std::vector <int> myApplication::getLevel(int level){
+const std::vector <int> myApplication::getLevel(const int level){
     return resources_.getLevel(level);
 }
 
