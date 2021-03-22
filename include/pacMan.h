41ae289ce879121cd491bdbfa77a94b4a3bf3a13
Abhishek Agarwal <agabhi001@gmail.com>
2021-03-22 19:33:37 +0530

    updated the pacman class

--- include/pacMan.h
+++ include/pacMan.h
@@ -7,21 +7,20 @@ class myApplication;
 
 class pacMan : public movable{
     private :
-        long long score_;
-        int small_pill_score_;
-        int big_pill_score_;
+        long long   score_;
+        int         small_pill_score_;
+        int         big_pill_score_;
     public :
         pacMan();
-        pacMan(std::vector <int>&, tileMap&, const std::string&, myApplication&, bool, int);
+        pacMan(const std::vector <int>&, const tileMap&, const std::string&, const myApplication&, const bool, const int);
         void updateScore();
-        void updateScore(int);
+        void updateScore(const int);
         void resetScore();
-        void setNewTexture(myApplication&, const std::string&);
+        void setNewTexture(const myApplication&, const std::string&);
         void glow(const std::string& event);
         void kill();
         const long long getScore();
         const int getLivesCount();
-
 };
 
 #endif //PAC_MAN_H
--- src/pacMan.cpp
+++ src/pacMan.cpp
@@ -1,6 +1,5 @@
 #include "pacMan.h"
 #include "myApplication.h"
-#include <iostream>
 
 pacMan::pacMan(){
     score_ = 0;
@@ -8,64 +7,40 @@ pacMan::pacMan(){
     big_pill_score_ = 50;
 }
 
-pacMan::pacMan(std::vector <int>& level, tileMap& tile_map, const std::string& texture_name, myApplication& app, bool random_spawn, int spawn_index) : movable(level, tile_map, "pacman_right_1", app, random_spawn, spawn_index){
-    score_ = 0;
-    small_pill_score_ = 5;
-    big_pill_score_ = 50;
+pacMan::pacMan(const std::vector <int>& level, const tileMap& tile_map, const std::string& texture_name, const myApplication& app, const bool random_spawn, const int spawn_index) : movable(level, tile_map, "pacman_right_1", app, random_spawn, spawn_index){
+    *this = pacMan();
 }
 
 void pacMan::updateScore(){
     score_ += 5;
 }
 
-void pacMan::updateScore(int index){
-    if (index == 0){
-        score_ += small_pill_score_;
-    }
-    else if (index == 2){
-        score_ += big_pill_score_;
-    }
-    else {
-        score_ += index;
-    }
+void pacMan::updateScore(const int index){
+    if (index == 0)         {score_ += small_pill_score_;}
+    else if (index == 2)    {score_ += big_pill_score_;}
+    else                    {score_ += index;}
 }
 
 void pacMan::resetScore(){
     score_ = 0;
 }
 
-void pacMan::setNewTexture(myApplication& app, const std::string& direction){
-    if (direction == "Up"){
-        setTexture(app, "pacman_up_1");
-    }
-    else if (direction == "Down"){
-        setTexture(app, "pacman_down_1");
-    }
-    else if (direction == "Right"){
-        setTexture(app, "pacman_right_1");
-    }
-    else if (direction == "Left"){
-        setTexture(app, "pacman_left_1");
-    }
+void pacMan::setNewTexture(const myApplication& app, const std::string& direction){
+    if (direction == "Up")          {this->setTexture(app, "pacman_up_1");}
+    else if (direction == "Down")   {this->setTexture(app, "pacman_down_1");}
+    else if (direction == "Right")  {this->setTexture(app, "pacman_right_1");}
+    else if (direction == "Left")   {this->setTexture(app, "pacman_left_1");}
 }
 
 void pacMan::glow(const std::string& event){
-    if (event == "normal"){
-        this->getSprite().setColor(sf::Color::White);
-    }
-    else if (event == "eat_small_pill"){
-        this->getSprite().setColor(sf::Color::Green);
-    }
-    else if (event == "eat_big_pill"){
-        this->getSprite().setColor(sf::Color::Red);
-    }
-    else if (event == "kill"){
-        this->getSprite().setColor(sf::Color::Red);
-    }
+    if (event == "normal")              {this->getSprite().setColor(sf::Color::White);}
+    else if (event == "eat_small_pill") {this->getSprite().setColor(sf::Color::Green);}
+    else if (event == "eat_big_pill")   {this->getSprite().setColor(sf::Color::Red);}
+    else if (event == "kill")           {this->getSprite().setColor(sf::Color::Red);}
 }
 
 void pacMan::kill(){
-    glow("kill");
+    this->glow("kill");
     is_alive_ = false;
 }
 
